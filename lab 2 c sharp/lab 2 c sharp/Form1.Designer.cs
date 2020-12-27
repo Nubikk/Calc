3ab4d34732b21e8e94ce70c2069d71eca8ba1ea8
Nubikk <60070139+Nubikk@users.noreply.github.com>
2020-12-27 13:54:21 +0300

    Add files via upload

---
+++ lab 2 c sharp/lab 2 c sharp/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿
+namespace lab_2_c_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number_1 = new System.Windows.Forms.Button();
+            this.number_9 = new System.Windows.Forms.Button();
+            this.number_3 = new System.Windows.Forms.Button();
+            this.number_0 = new System.Windows.Forms.Button();
+            this.number_6 = new System.Windows.Forms.Button();
+            this.number_5 = new System.Windows.Forms.Button();
+            this.number_2 = new System.Windows.Forms.Button();
+            this.number_8 = new System.Windows.Forms.Button();
+            this.number_7 = new System.Windows.Forms.Button();
+            this.number_4 = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multiplay = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.divided = new System.Windows.Forms.Button();
+            this.left_skob = new System.Windows.Forms.Button();
+            this.right_skob = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.point = new System.Windows.Forms.Button();
+            this.erase = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.znach_znak = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // number_1
+            // 
+            this.number_1.Location = new System.Drawing.Point(76, 63);
+            this.number_1.Name = "number_1";
+            this.number_1.Size = new System.Drawing.Size(39, 26);
+            this.number_1.TabIndex = 1;
+            this.number_1.Text = "1";
+            this.number_1.UseVisualStyleBackColor = true;
+            this.number_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // number_9
+            // 
+            this.number_9.Location = new System.Drawing.Point(166, 127);
+            this.number_9.Name = "number_9";
+            this.number_9.Size = new System.Drawing.Size(39, 26);
+            this.number_9.TabIndex = 2;
+            this.number_9.Text = "9";
+            this.number_9.UseVisualStyleBackColor = true;
+            this.number_9.Click += new System.EventHandler(this.number_9_Click);
+            // 
+            // number_3
+            // 
+            this.number_3.Location = new System.Drawing.Point(166, 63);
+            this.number_3.Name = "number_3";
+            this.number_3.Size = new System.Drawing.Size(39, 26);
+            this.number_3.TabIndex = 3;
+            this.number_3.Text = "3";
+            this.number_3.UseVisualStyleBackColor = true;
+            this.number_3.Click += new System.EventHandler(this.number_3_Click);
+            // 
+            // number_0
+            // 
+            this.number_0.Location = new System.Drawing.Point(121, 159);
+            this.number_0.Name = "number_0";
+            this.number_0.Size = new System.Drawing.Size(39, 26);
+            this.number_0.TabIndex = 4;
+            this.number_0.Text = "0";
+            this.number_0.UseVisualStyleBackColor = true;
+            this.number_0.Click += new System.EventHandler(this.number_0_Click);
+            // 
+            // number_6
+            // 
+            this.number_6.Location = new System.Drawing.Point(166, 95);
+            this.number_6.Name = "number_6";
+            this.number_6.Size = new System.Drawing.Size(39, 26);
+            this.number_6.TabIndex = 5;
+            this.number_6.Text = "6";
+            this.number_6.UseVisualStyleBackColor = true;
+            this.number_6.Click += new System.EventHandler(this.number_6_Click);
+            // 
+            // number_5
+            // 
+            this.number_5.Location = new System.Drawing.Point(121, 95);
+            this.number_5.Name = "number_5";
+            this.number_5.Size = new System.Drawing.Size(39, 26);
+            this.number_5.TabIndex = 6;
+            this.number_5.Text = "5";
+            this.number_5.UseVisualStyleBackColor = true;
+            this.number_5.Click += new System.EventHandler(this.number_5_Click);
+            // 
+            // number_2
+            // 
+            this.number_2.Location = new System.Drawing.Point(121, 63);
+            this.number_2.Name = "number_2";
+            this.number_2.Size = new System.Drawing.Size(39, 26);
+            this.number_2.TabIndex = 7;
+            this.number_2.Text = "2";
+            this.number_2.UseVisualStyleBackColor = true;
+            this.number_2.Click += new System.EventHandler(this.number_2_Click);
+            // 
+            // number_8
+            // 
+            this.number_8.Location = new System.Drawing.Point(121, 127);
+            this.number_8.Name = "number_8";
+            this.number_8.Size = new System.Drawing.Size(39, 26);
+            this.number_8.TabIndex = 8;
+            this.number_8.Text = "8";
+            this.number_8.UseVisualStyleBackColor = true;
+            this.number_8.Click += new System.EventHandler(this.number_8_Click);
+            // 
+            // number_7
+            // 
+            this.number_7.Location = new System.Drawing.Point(76, 127);
+            this.number_7.Name = "number_7";
+            this.number_7.Size = new System.Drawing.Size(39, 26);
+            this.number_7.TabIndex = 9;
+            this.number_7.Text = "7";
+            this.number_7.UseVisualStyleBackColor = true;
+            this.number_7.Click += new System.EventHandler(this.number_7_Click);
+            // 
+            // number_4
+            // 
+            this.number_4.Location = new System.Drawing.Point(76, 95);
+            this.number_4.Name = "number_4";
+            this.number_4.Size = new System.Drawing.Size(39, 26);
+            this.number_4.TabIndex = 10;
+            this.number_4.Text = "4";
+            this.number_4.UseVisualStyleBackColor = true;
+            this.number_4.Click += new System.EventHandler(this.number_4_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(76, 191);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(39, 26);
+            this.minus.TabIndex = 11;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // multiplay
+            // 
+            this.multiplay.Location = new System.Drawing.Point(166, 159);
+            this.multiplay.Name = "multiplay";
+            this.multiplay.Size = new System.Drawing.Size(39, 26);
+            this.multiplay.TabIndex = 12;
+            this.multiplay.Text = "*";
+            this.multiplay.UseVisualStyleBackColor = true;
+            this.multiplay.Click += new System.EventHandler(this.multiplay_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(76, 159);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(39, 26);
+            this.plus.TabIndex = 13;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // divided
+            // 
+            this.divided.Location = new System.Drawing.Point(166, 191);
+            this.divided.Name = "divided";
+            this.divided.Size = new System.Drawing.Size(39, 26);
+            this.divided.TabIndex = 14;
+            this.divided.Text = "/";
+            this.divided.UseVisualStyleBackColor = true;
+            this.divided.Click += new System.EventHandler(this.divided_Click);
+            // 
+            // left_skob
+            // 
+            this.left_skob.Location = new System.Drawing.Point(99, 223);
+            this.left_skob.Name = "left_skob";
+            this.left_skob.Size = new System.Drawing.Size(39, 26);
+            this.left_skob.TabIndex = 15;
+            this.left_skob.Text = "(";
+            this.left_skob.UseVisualStyleBackColor = true;
+            this.left_skob.Click += new System.EventHandler(this.left_skob_Click);
+            // 
+            // right_skob
+            // 
+            this.right_skob.Location = new System.Drawing.Point(144, 223);
+            this.right_skob.Name = "right_skob";
+            this.right_skob.Size = new System.Drawing.Size(39, 26);
+            this.right_skob.TabIndex = 16;
+            this.right_skob.Text = ")";
+            this.right_skob.UseVisualStyleBackColor = true;
+            this.right_skob.Click += new System.EventHandler(this.right_skob_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 45);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // point
+            // 
+            this.point.Location = new System.Drawing.Point(121, 191);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(39, 26);
+            this.point.TabIndex = 18;
+            this.point.Text = ".";
+            this.point.UseVisualStyleBackColor = true;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // erase
+            // 
+            this.erase.Location = new System.Drawing.Point(6, 63);
+            this.erase.Name = "erase";
+            this.erase.Size = new System.Drawing.Size(64, 34);
+            this.erase.TabIndex = 19;
+            this.erase.Text = "C";
+            this.erase.UseVisualStyleBackColor = true;
+            this.erase.Click += new System.EventHandler(this.erase_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(211, 63);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(76, 34);
+            this.result.TabIndex = 20;
+            this.result.Text = "=";
+            this.result.UseVisualStyleBackColor = true;
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 21;
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // znach_znak
+            // 
+            this.znach_znak.Location = new System.Drawing.Point(99, 255);
+            this.znach_znak.Name = "znach_znak";
+            this.znach_znak.Size = new System.Drawing.Size(84, 34);
+            this.znach_znak.TabIndex = 22;
+            this.znach_znak.Text = "+ / --";
+            this.znach_znak.UseVisualStyleBackColor = true;
+            this.znach_znak.Click += new System.EventHandler(this.znach_znak_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(299, 312);
+            this.Controls.Add(this.znach_znak);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.erase);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.right_skob);
+            this.Controls.Add(this.left_skob);
+            this.Controls.Add(this.divided);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.multiplay);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.number_4);
+            this.Controls.Add(this.number_7);
+            this.Controls.Add(this.number_8);
+            this.Controls.Add(this.number_2);
+            this.Controls.Add(this.number_5);
+            this.Controls.Add(this.number_6);
+            this.Controls.Add(this.number_0);
+            this.Controls.Add(this.number_3);
+            this.Controls.Add(this.number_9);
+            this.Controls.Add(this.number_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button number_1;
+        private System.Windows.Forms.Button number_9;
+        private System.Windows.Forms.Button number_3;
+        private System.Windows.Forms.Button number_0;
+        private System.Windows.Forms.Button number_6;
+        private System.Windows.Forms.Button number_5;
+        private System.Windows.Forms.Button number_2;
+        private System.Windows.Forms.Button number_8;
+        private System.Windows.Forms.Button number_7;
+        private System.Windows.Forms.Button number_4;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multiplay;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button divided;
+        private System.Windows.Forms.Button left_skob;
+        private System.Windows.Forms.Button right_skob;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button point;
+        private System.Windows.Forms.Button erase;
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button znach_znak;
+    }
+}
+
---
+++ lab 2 c sharp/lab 2 c sharp/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_2_c_sharp
+{
+    
+    public partial class Form1 : Form
+    {private void calculate()
+    {
+        switch (op)
+        {
+            case 1:
+                b = a + float.Parse(textBox1.Text);
+                textBox1.Text = b.ToString();
+                break;
+            case 2:
+                b = a - float.Parse(textBox1.Text);
+                textBox1.Text = b.ToString();
+                break;
+            case 3:
+                b = a * float.Parse(textBox1.Text);
+                textBox1.Text = b.ToString();
+                break;
+            case 4:
+
+                if (float.Parse(textBox1.Text) == 0)
+                { textBox1.Text = "Делить на 0 нельзя!"; }
+                else
+                {
+                    b = a / float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+        float a,b;
+        int op;
+        bool znak = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            op = 1;
+            label1.Text = a.ToString() + "+";
+            znak = true;
+        }
+
+        private void number_2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+
+        }
+
+    
+
+        private void number_3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void number_4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void number_5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void number_6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void number_7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void number_8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void number_9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void number_0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void multiplay_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            op = 1;
+            label1.Text = a.ToString() + "*";
+            znak = true;
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            op = 1;
+            label1.Text = a.ToString() + "-";
+            znak = false;
+        }
+
+        private void divided_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            op = 1;
+            label1.Text = a.ToString() + "/";
+            znak = true;
+        }
+
+        private void left_skob_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "(";
+        }
+
+        private void right_skob_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ")";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_Click(object sender, EventArgs e)
+        {
+            calculate();
+            label1.Text = "";
+        }
+
+        private void point_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ",";
+        }
+
+        private void znach_znak_Click(object sender, EventArgs e) /* Если znak=true, мы меняем знак операнда в
+                                                                   * textBox1, а если false, то 
+                                                                   * знак числа удаляется из текстового поля.
+            */{
+            if(znak == true)
+            {
+                textBox1.Text = "-" + textBox1.Text;
+                znak = false;
+            }
+            else if (znak == false)
+            {
+                textBox1.Text = textBox1.Text.Replace("-", "");
+                znak = true;
+            }
+        }
+
+        private void erase_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label1.Text = "";
+        }
+    }
+}
